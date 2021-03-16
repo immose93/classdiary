7b714f0eb0396617ef86f343d7e77c9eefd741b8
immose93 <immoses93@gmail.com>
2021-03-16 18:12:46 +0900

    Initial commit

---
+++ src/main/java/com/moses/classdiary/ClassdiaryApplication.java
@@ -0,0 +1,13 @@
+package com.moses.classdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassdiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassdiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moses/classdiary/ClassdiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moses.classdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassdiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
