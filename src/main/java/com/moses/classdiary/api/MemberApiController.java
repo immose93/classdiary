7dea5241d879b13e856d5ddd9bfde5bbab75d6fd
immose93 <immoses93@gmail.com>
2021-05-18 20:50:23 +0900

    회원가입 API 관련 부분 추가 및 수정 (@Valid 관련 어노테이션을 Member에서 SignUpMemberDto로 옮김)

--- src/main/java/com/moses/classdiary/api/MemberApiController.java
+++ src/main/java/com/moses/classdiary/api/MemberApiController.java
@@ -1,16 +1,14 @@
 package com.moses.classdiary.api;
 
-import com.moses.classdiary.dto.member.*;
-import com.moses.classdiary.entity.Authority;
+import com.moses.classdiary.dto.member.SignUpMemberDto;
 import com.moses.classdiary.entity.Member;
 import com.moses.classdiary.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
 import javax.validation.Valid;
-import java.util.Collections;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,20 +16,26 @@ import java.util.Collections;
 public class MemberApiController {
     private final MemberService memberService;
 
-    @PostMapping("/member/signup")
-    public SignUpMemberResponse signup(@RequestBody @Valid SignUpMemberRequest request){
-        Member member = new Member();
-        member.setUsername(request.getUsername());
-        member.setPassword(request.getPassword());
-        member.setName(request.getName());
-        member.setEmail(request.getEmail());
-        member.setSchoolName(request.getSchoolName());
-        member.setGrade(request.getGrade());
-        member.setClassNum(request.getClassNum());
-        member.setAuthorities(Collections.singleton(new Authority("ROLE_USER")));
+    /**
+     * 회원가입 API
+     * @param signUpMemberDto - 회원가입 정보가 담긴 DTO
+     * @return Member가 담긴 ResponseEntity
+     */
+    @PostMapping("/signup")
+    public ResponseEntity<Member> signup(@RequestBody @Valid SignUpMemberDto signUpMemberDto){
+        return ResponseEntity.ok(memberService.signup(signUpMemberDto));
+    }
 
-        Long id = memberService.signup(member);
-        return new SignUpMemberResponse(id);
+    @GetMapping("/user")
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+    public ResponseEntity<Member> getMyUserInfo(){
+        return ResponseEntity.ok(memberService.getMyUserWithAuthorities().get());
+    }
+
+    @GetMapping("/user/{username}")
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    public ResponseEntity<Member> getUserInfo(@PathVariable String username){
+        return ResponseEntity.ok(memberService.getUserWithAuthorities(username).get());
     }
 
 }
