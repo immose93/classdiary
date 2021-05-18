dd13c41cfb103f71895768b478d0553a7852a0ac
immose93 <immoses93@gmail.com>
2021-05-18 19:46:29 +0900

    DB 초기 데이터 추가 부분 수정 (Authority 2개 추가 + Member 비밀번호 암호화 후 추가)

--- src/main/java/com/moses/classdiary/InitDb.java
+++ src/main/java/com/moses/classdiary/InitDb.java
@@ -1,12 +1,15 @@
 package com.moses.classdiary;
 
+import com.moses.classdiary.entity.Authority;
 import com.moses.classdiary.entity.Member;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
+import java.util.Collections;
 
 @Component
 @RequiredArgsConstructor
@@ -24,15 +27,23 @@ public class InitDb {
     @RequiredArgsConstructor
     static class InitService {
         private final EntityManager em;
+        private final PasswordEncoder passwordEncoder;
+
         public  void dbInit1(){
+
+            em.persist(new Authority("ROLE_USER"));
+            em.persist(new Authority("ROLE_ADMIN"));
+
             Member member = new Member();
             member.setUsername("test");
-            member.setPassword("test123@");
+            member.setPassword(passwordEncoder.encode("test123@"));
             member.setName("테스트");
             member.setEmail("test@gmail.com");
             member.setSchoolName("테스트초");
             member.setGrade(5);
             member.setClassNum(3);
+            member.setAuthorities(Collections.singleton(new Authority("ROLE_USER")));
+            member.setActivated(true);
             em.persist(member);
         }
     }
